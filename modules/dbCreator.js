2d9243f29087f5a05ef91ef7f27c0ff8ca0909f8
Şahin Nazlı <snazli@cozum-makina.com>
2022-02-02 12:11:51 +0300

    Delete layouts

--- modules/dbCreator.js
+++ modules/dbCreator.js
@@ -2,9 +2,8 @@ let db = require("./dbConnect").db;
 const dbCreator = ()=>{
 
 db.serialize(function() {
-  this.run('CREATE TABLE IF NOT EXISTS "users" ("id" INTEGER,"user_name" TEXT,"user_pass" TEXT,"last_online" TEXT,"created_at" TEXT,"country"	TEXT,PRIMARY KEY("id" AUTOINCREMENT))');
-  this.run('CREATE TABLE IF NOT EXISTS "groups" ("id" INTEGER,"group_name" TEXT,"group_pic_url"	TEXT,"users" TEXT,PRIMARY KEY("id" AUTOINCREMENT))');
-  this.run('CREATE TABLE IF NOT EXISTS "messages" ("id"	INTEGER,"send_from"	INTEGER,"send_to" INTEGER,"read_status" INTEGER,"read_users" TEXT,PRIMARY KEY("id" AUTOINCREMENT))');
+  this.run('CREATE TABLE IF NOT EXISTS "users" ("id"	INTEGER,"user_name"	TEXT,"user_pass"	TEXT,"last_online"	INTEGER,"created_at"	INTEGER,"country"	TEXT,"language"	TEXT,"email"	TEXT,PRIMARY KEY("id" AUTOINCREMENT))');
+  this.run('CREATE TABLE IF NOT EXISTS "messages" ("id"	INTEGER,"send_from"	INTEGER,"send_to"	INTEGER,"read_status"	INTEGER DEFAULT 0,"message"	TEXT,"message_time"	INTEGER,PRIMARY KEY("id" AUTOINCREMENT))');
   this.run('CREATE TABLE IF NOT EXISTS "events" ("id" INTEGER,"evet_descp" TEXT,"user_id" INTEGER,"event_time" TEXT,PRIMARY KEY("id" AUTOINCREMENT))');
 });
 
